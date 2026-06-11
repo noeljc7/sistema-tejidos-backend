5384e569ea45ba7b15124af1267025a733d6eaf4
noeljc7 <juceverson8@gmail.com>
2026-06-11 16:00:59 -0600

    Fix: Eliminar configuracion experimental invalida en next.config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    allowedDevOrigins: ['10.226.191.5', '10.226.191.60', '10.1.19.2']
-  }
+  /* config options here */
 };
 
 module.exports = nextConfig;
